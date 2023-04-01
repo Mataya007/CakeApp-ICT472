cadfbe06ef773f8a55b679e3c117003d13091e84
Japhet <japhetgondwe20@gmail.com>
2023-04-01 11:58:40 +0200

    modified:   Cake/redcodecakers/lib/main.dart

--- Cake/redcodecakers/lib/main.dart
+++ Cake/redcodecakers/lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main() 
+{
   runApp(CakeApp());
 }
 
-class CakeApp extends StatelessWidget {
+class CakeApp extends StatelessWidget 
+{
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) 
+  {
+    return MaterialApp
+    (
       title: 'CakeApp-ICT472',
-      theme: ThemeData(
+      theme: ThemeData
+      (
         primarySwatch: Colors.pink,
       ),
       home: LoginPage(),
@@ -17,32 +22,45 @@ class CakeApp extends StatelessWidget {
   }
 }
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatelessWidget 
+{
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+  Widget build(BuildContext context) 
+  {
+    return Scaffold
+    (
+      appBar: AppBar
+      (
         title: Text('Login/Registration'),
       ),
-      body: Center(
-        child: Column(
+      body: Center
+      (
+        child: Column
+        (
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            RaisedButton(
+          children: 
+          [
+            ElevatedButton
+            (
               child: Text('Register'),
-              onPressed: () {
+              onPressed: () 
+              {
                 // Navigate to registration page
               },
             ),
-            RaisedButton(
+            ElevatedButton
+            (
               child: Text('Login'),
-              onPressed: () {
+              onPressed: () 
+              {
                 // Navigate to login page
               },
-            ),
-            RaisedButton(
+            ), 
+            ElevatedButton
+            (
               child: Text('About/Cake App'),
-              onPressed: () {
+              onPressed: () 
+              {
                 // Navigate to about page
               },
             ),
@@ -53,24 +71,36 @@ class LoginPage extends StatelessWidget {
   }
 }
 
-class CakeViewPage extends StatelessWidget {
+class CakeViewPage extends StatelessWidget 
+{
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+  Widget build(BuildContext context) 
+  {
+    return Scaffold
+    (
+      appBar: AppBar
+      (
         title: Text('Cake View'),
       ),
-      body: Center(
-        child: Column(
+      body: Center
+      (
+        child: Column
+        (
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: InputDecoration(
+          children: 
+          [
+            TextField
+            (
+              decoration: InputDecoration
+              (
                 hintText: 'Search for cakes',
               ),
             ),
-            TabBar(
-              tabs: [
+            
+            TabBar
+            (
+              tabs: 
+              [
                 Tab(text: 'Home'),
                 Tab(text: 'About'),
                 Tab(text: 'Bakers near you'),
@@ -83,28 +113,38 @@ class CakeViewPage extends StatelessWidget {
   }
 }
 
-class CakeDetailsPage extends StatelessWidget {
+class CakeDetailsPage extends StatelessWidget 
+{
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+  Widget build(BuildContext context) 
+  {
+    return Scaffold
+    (
+      appBar: AppBar
+      (
         title: Text('Cake Details'),
       ),
-      body: Center(
-        child: Column(
+      body: Center
+      (
+        child: Column
+        (
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+          children: 
+          [
             Text('About Cake'),
             Text('About Bakers'),
-            RaisedButton(
+            ElevatedButton(
               child: Text('Book Baker'),
-              onPressed: () {
+              onPressed: () 
+              {
                 // Navigate to booking page
               },
             ),
-            RaisedButton(
+            ElevatedButton
+            (
               child: Text('Comments/Reviews/Reactions'),
-              onPressed: () {
+              onPressed: () 
+              {
                 // Navigate to comments page
               },
             ),
